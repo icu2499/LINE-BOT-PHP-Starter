b3e4dd7a1c35de555e71e1c3110bb6526d327a1b
icu2499 <31514213+icu2499@users.noreply.github.com>
2017-08-31 22:48:32 +0700

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . KgjDXVFpW7EuU9WOFSy0DzSieKFavlz0IzI7lHhtXLk+YMqXuNRVcFl34qBguUsNb5/0H7uL8swwEj75qFQRRIHKJSw9PnQ8ltrbYrbejwac9T9LKHjZo34231E+4Qhf6wzy7ZJr/3J0N1Dfm4qzjQdB04t89/1O/w1cDnyilFU=);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
